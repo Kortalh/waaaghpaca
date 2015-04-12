6d3adf6f89173a9315df7483d37bdbced0054637
Kortalh <kortalh@gmail.com>
2015-04-12 17:14:51 -0500

    Added Bootstrap & Sass support

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'wpca-init',
+    modulePrefix: 'wpca',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
