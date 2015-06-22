379c9f47d75918dbe4ae9692bfa4fd53caa02763
Kortalh <kortalh@gmail.com>
2015-06-22 00:01:36 -0500

    Bugfix for Firefox interpretation of MySQL timestamps.

--- app/helpers/date-format.js
+++ app/helpers/date-format.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 export function timeFormat(params/*, hash*/) {
 
-	var months	= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-		date	= new Date(params[0]),
-		month	= months[date.getMonth()];
+
+console.log('params:', params);
+
+		// Used for converting numerical months to strings
+	var months		= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		// Used for Firefox timestamp interpretation
+		timestamp	= params[0].split(/[- :]/),
+		date		= new Date(timestamp[0], timestamp[1]-1, timestamp[2], timestamp[3], timestamp[4], timestamp[5]),
+		month		= months[date.getMonth()];
 
 	return month + ' ' + date.getDate() + ', ' + date.getFullYear();
 }
